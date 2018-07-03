8e5b0605d7620f722c9b87a472d1385957822e6e
René Schultz Madsen <rm@microting.com>
2018-07-03 10:46:45 +0200

    Fixing company name and assambly name

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EformBase.Pn")]
+[assembly: AssemblyTitle("Microting.eFormApi.BasePn")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EformBase.Pn")]
+[assembly: AssemblyCompany("Microting A/S")]
+[assembly: AssemblyProduct("Microting.eFormApi.BasePn")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
