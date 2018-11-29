9dd82141d702d53a3ab90c7f48181ae9270cbb87
René Schultz Madsen <rm@microting.dk>
2018-11-29 14:40:20 +0100

    Updating: - Microsoft.AspNetCore.Identity.EntityFrameworkCore to 2.1.6 - Microsoft.AspNetCore.All to 2.1.6
    Updating the ConnectionStringSdk model to reflect the new structure.

--- eFormApi.BasePn/Infrastructure/Models/Settings/Initial/GeneralAppSetupSettingsModel.cs
+++ eFormApi.BasePn/Infrastructure/Models/Settings/Initial/GeneralAppSetupSettingsModel.cs
@@ -3,5 +3,6 @@ namespace Microting.eFormApi.BasePn.Infrastructure.Models.Settings.Initial
     public class GeneralAppSetupSettingsModel
     {
         public string DefaultLocale { get; set; }
+        public int CustomerNo { get; set; }
     }
 }
