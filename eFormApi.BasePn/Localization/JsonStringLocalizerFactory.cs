7cececef3da45a5a533fd0914826d86b0cae82d4
Leonid <em-atf@mail.ru>
2018-12-11 21:38:19 +0200

    Added json localization

---
+++ eFormApi.BasePn/Localization/JsonStringLocalizerFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Extensions.Localization;
+
+namespace Microting.eFormApi.BasePn.Localization
+{
+    public class JsonStringLocalizerFactory : IStringLocalizerFactory
+    {
+        public IStringLocalizer Create(Type resourceSource)
+        {
+            return new JsonStringLocalizer(resourceSource);
+        }
+
+        public IStringLocalizer Create(string baseName, string location)
+        {
+            return new JsonStringLocalizer();
+        }
+    }
+}
