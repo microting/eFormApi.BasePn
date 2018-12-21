752ed163203e3efcffcf777f9363dad66f977792
Gid <gid733@ya.ru>
2018-12-21 17:38:04 +0200

    Updates

---
+++ eFormApi.BasePn/Infrastructure/Models/SearchableList/AdvEntitySearchableGroupImportModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Microting.eFormApi.BasePn.Infrastructure.Models.SearchableList
+{
+    public class AdvEntitySearchableGroupImportModel
+    {
+        public string ImportString { get; set; }
+    }
+}
---
+++ eFormApi.BasePn/Infrastructure/Models/Settings/Initial/GeneralAppSetupSettingsModel.cs
@@ -0,0 +1,8 @@
+namespace Microting.eFormApi.BasePn.Infrastructure.Models.Settings.Initial
+{
+    public class GeneralAppSetupSettingsModel
+    {
+        public string DefaultLocale { get; set; }
+        public int CustomerNo { get; set; }
+    }
+}
