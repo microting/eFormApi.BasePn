d1d7aff5fa2701c032da8cf00e30ae703d6c5b4c
Leonid <em-atf@mail.ru>
2018-12-21 13:49:35 +0200

    Remove models

--- eFormApi.BasePn/Infrastructure/Models/SiteNameModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Microting.eFormApi.BasePn.Infrastructure.Models
-{
-    public class SiteNameModel
-    {
-        public int Id { get; set; }
-        public string SiteName { get; set; }
-    }
-}
