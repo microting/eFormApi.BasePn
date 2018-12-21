03bfb5ec3676de2f55102de56a32f4daaeb47106
Leonid <em-atf@mail.ru>
2018-12-21 18:01:52 +0200

    Updates

--- eFormApi.BasePn/Infrastructure/Models/Settings/Initial/GeneralAppSetupSettingsModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Microting.eFormApi.BasePn.Infrastructure.Models.Settings.Initial
-{
-    public class GeneralAppSetupSettingsModel
-    {
-        public string DefaultLocale { get; set; }
-        public int CustomerNo { get; set; }
-    }
-}
--- eFormApi.BasePn/Infrastructure/Models/Templates/DeployCheckbox.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Microting.eFormApi.BasePn.Infrastructure.Models.Templates
-{
-    public class DeployCheckbox
-    {
-        public int Id { get; set; }
-        public bool IsChecked { get; set; }
-    }
-}
