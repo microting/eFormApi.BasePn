2fb8db15eec4cac23f725e60ff6493185ddb6638
Leonid <em-atf@mail.ru>
2018-12-21 18:01:52 +0200

    Updates

--- eFormApi.BasePn/Infrastructure/Models/SelectableList/AdvEntitySelectableGroupImportModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Microting.eFormApi.BasePn.Infrastructure.Models.SelectableList
-{
-    public class AdvEntitySelectableGroupImportModel
-    {
-        public string ImportString { get; set; }
-    }
-}
