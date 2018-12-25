d1dee65fa034411116d3050152feacb2119b5a74
Leonid <em-atf@mail.ru>
2018-12-25 18:47:37 +0200

    Refactor options

--- eFormApi.BasePn/Infrastructure/Helpers/WritableOptions/IWritableOptions.cs
+++ eFormApi.BasePn/Infrastructure/Helpers/WritableOptions/IWritableOptions.cs
@@ -5,6 +5,6 @@ namespace Microting.eFormApi.BasePn.Infrastructure.Helpers.WritableOptions
 {
     public interface IWritableOptions<out T> : IOptionsSnapshot<T> where T : class, new()
     {
-        void Update(Action<T> applyChanges);
+        void UpdateFile(Action<T> applyChanges);
     }
 }
