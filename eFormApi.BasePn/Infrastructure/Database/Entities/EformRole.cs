8ae69c5747c2cc4bfead6b0abcee7a6c6ba14ee0
René Schultz Madsen <rm@microting.dk>
2019-03-13 14:08:53 +0100

    Cleaning up code layout.

--- eFormApi.BasePn/Infrastructure/Database/Entities/EformRole.cs
+++ eFormApi.BasePn/Infrastructure/Database/Entities/EformRole.cs
@@ -6,6 +6,7 @@ namespace Microting.eFormApi.BasePn.Infrastructure.Database.Entities
     public class EformRole : IdentityRole<int>
     {
         public const string Admin = "admin";
+        
         public const string User = "user";
 
         public virtual ICollection<EformUserRole> UserRoles { get; set; }
--- eFormApi.BasePn/Infrastructure/Database/Entities/EformUserRole.cs
+++ eFormApi.BasePn/Infrastructure/Database/Entities/EformUserRole.cs
@@ -5,6 +5,7 @@ namespace Microting.eFormApi.BasePn.Infrastructure.Database.Entities
     public class EformUserRole : IdentityUserRole<int>
     {
         public virtual EformUser User { get; set; }
+        
         public virtual EformRole Role { get; set; }
     }
 }
