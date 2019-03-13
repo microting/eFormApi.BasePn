96f584bb22e7f22bd3bc4ea312df8505809e89e4
René Schultz Madsen <rm@microting.dk>
2019-03-13 14:08:53 +0100

    Cleaning up code layout.

--- eFormApi.BasePn/Infrastructure/Database/Entities/PluginConfigurationValue.cs
+++ eFormApi.BasePn/Infrastructure/Database/Entities/PluginConfigurationValue.cs
@@ -3,6 +3,7 @@
     public class PluginConfigurationValue 
     {
         public string Id { get; set; }
+        
         public string Value { get; set; }
     }
 }
--- eFormApi.BasePn/Infrastructure/Database/Entities/PluginConfigurationVersion.cs
+++ eFormApi.BasePn/Infrastructure/Database/Entities/PluginConfigurationVersion.cs
@@ -3,7 +3,9 @@
     public class PluginConfigurationVersion
     {
         public string Id { get; set; }
+        
         public string Value { get; set; }
+        
         public int Version { get; set; }
     }
 }
