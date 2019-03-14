373aa169a9dd3e23def9e3abeaa3dc4c0176f383
René Schultz Madsen <rm@microting.dk>
2019-03-14 13:06:07 +0100

    Adding the missing IPluginDbContext

--- eFormApi.BasePn/Abstractions/IPluginDbContext.cs
+++ eFormApi.BasePn/Abstractions/IPluginDbContext.cs
@@ -1,15 +1,11 @@
-﻿using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microting.eFormApi.BasePn.Infrastructure.Database.Entities;
 
 namespace Microting.eFormApi.BasePn.Abstractions
 {
-    public interface IPluginDbContext 
+    public interface IPluginDbContext
     {
         DbSet<PluginConfigurationValue> PluginConfigurationValues { get; set; }
-        DbSet<PluginConfigurationVersion> PluginConfigurationVersions { get; set; }
-        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken));
-        int SaveChanges();
+        DbSet<PluginConfigurationValueVersion> PluginConfigurationValueVersions { get; set; }
     }
 }
