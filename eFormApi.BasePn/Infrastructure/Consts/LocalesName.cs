d687a3ce1c07965ffa3b1c07a56f3bfc233e1ce5
oleksiygarnik <oleksiy.garnik27@gmail.com>
2020-10-30 15:46:04 +0200

    Added additional field for plugin menu items and added consts in infrastructure

---
+++ eFormApi.BasePn/Infrastructure/Consts/LocalesName.cs
@@ -0,0 +1,9 @@
+﻿namespace Microting.eFormApi.BasePn.Infrastructure.Consts
+{
+    public static class LocaleNames
+    {
+        public const string English = "en-US";
+        public const string Danish = "da-DK";
+        public const string German = "de-DE";
+    }
+}
