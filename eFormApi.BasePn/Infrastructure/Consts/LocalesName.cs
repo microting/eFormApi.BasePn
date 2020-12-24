51417e6b06682b7437549d9915ec92d01f8beb6c
sanioooook <sahok6264@gmail.com>
2020-12-24 14:51:49 +0200

    Add new const for new language locale

--- eFormApi.BasePn/Infrastructure/Consts/LocalesName.cs
+++ eFormApi.BasePn/Infrastructure/Consts/LocalesName.cs
@@ -5,5 +5,6 @@
         public const string English = "en-US";
         public const string Danish = "da-DK";
         public const string German = "de-DE";
+        public const string Ukrainian = "uk-UA";
     }
 }
